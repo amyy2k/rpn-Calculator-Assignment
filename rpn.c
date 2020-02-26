7a7e5f15997913e7f0b0abfba18a0ba1c8c6303e
amyy2k <60591552+amyy2k@users.noreply.github.com>
2020-02-26 12:45:45 -0800

    Update rpn.c

--- rpn.c
+++ rpn.c
@@ -9,12 +9,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
-#include<math.h>
+#include <math.h>
+#include <string.h>
 
 #define MAXOP   100    /* max size of operand/operator */
 #define NUMBER '0'     /* signal that a number was found */
 #define MAXVAL  100
 #define MAXVAR 26
+#define MATH 'n'
+#define VARIABLE 'v'
+
 
 size_t sp = 0;   // aka unsigned long -- printf using %zu
 double val[MAXVAL];   // stack of values
@@ -38,8 +42,19 @@ int getop(char* s) {
   while ((s[0] = c = getch_()) == ' ' || c == '\t') { }  // skip whitespace
   s[1] = '\0';
 
+  if(c >= 'a' && c <= 'z'){
+      i = 0;
+      while (isalpha(s[++i] = c = getch_()));
+      s[i] = '\0';
+      return MATH;
+    }
+  if (c == '=' || c == '?'){
+     i = 0;
+     while (isalpha(s[++i] = c = getch_()));
+     s[i] = '\0';
+     return VARIABLE;
+   }
   if (!isdigit(c) && c != '.') { return c; }  // if not a digit, return
-
   i = 0;
   if (isdigit(c)) {  // get digits before '.'
     while (isdigit(s[++i] = c = getch_())) { }
@@ -72,10 +87,31 @@ void push(double f) {
   val[sp++] = f;
 }
 
+void  math(char* s){
+  double op1, op2, result = 0;
+
+  if (strcmp(s, "pow") == 0) {
+    op2 = pop();
+    op1 = pop();
+    result = pow(op1, op2);
+  }
+  else if (strcmp(s, "sin") == 0){ result = sin(pop()); }
+  else if (strcmp(s, "cos") == 0){ result = cos(pop()); }
+  else if (strcmp(s, "exp") == 0){ result = exp(pop()); }
+  push(result);
+  printf("%.2f\n", result);
+}
+void variable(int v, double var[]){
+  pop();
+  if(v >= 'A' && v <= 'Z') {
+  var[v -'A'] = pop();
+  }
+}
+
 void rpn(void) {
-  int type, v = 0;
+  int type, v;
   double op2, op1, value;
-  int var[MAXVAR];
+  double var[MAXVAR];
   char s[BUFSIZ];
 
   while ((type = getop(s)) != EOF) {
@@ -85,6 +121,8 @@ void rpn(void) {
       printf("\t%.8g\n",value);
       break;
       case NUMBER:  push(atof(s));              break;
+      case MATH:    math(s);
+      case VARIABLE: variable(v, var);
       case '+':     push(pop() + pop());        break;
       case '*':     push(pop() * pop());        break;
       case '-':     push(-(pop() - pop()));     break;
@@ -108,20 +146,12 @@ void rpn(void) {
       push(op2);
       push(op2);
       break;
-      case '$': //swap function
+      case 's': //swap function
       op2 = pop();
       op1 = pop();
        push(op2);
        push(op1);
        break;
-      case 's': //SIN function
-      push(sin(pop())); break;
-      case '^': //raise to the power
-      op2 = pop();
-      push(pow(pop(), op2));
-      break;
-      case 'e': //exponent function
-      push(exp(pop())); break;
       case '='://variable function
       pop();
     if(v >= 'A' && v <= 'Z'){
